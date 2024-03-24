9f340e657da62757683e0667b7fbab8af2411fc9
crimescene118 <163518810+crimescene118@users.noreply.github.com>
2024-03-25 05:53:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.querySelectorAll('.btn').forEach(button => {
     button.addEventListener('click', function() {
         handleClick(this.getAttribute('data-id'));
+        // 버튼을 클릭했을 때, 모든 버튼을 숨기고 뒤로 가기 버튼을 보여줌
+        document.querySelectorAll('.btn').forEach(btn => btn.classList.add('hidden'));
+        document.getElementById('backButton').classList.remove('hidden');
+        document.getElementById('newImageButton').classList.remove('hidden');
     });
 });
 
@@ -12,39 +16,24 @@ function handleClick(buttonId) {
         4: "인스타게시글.jpg"
     };
 
-    var imageUrl = images[buttonId]; // 버튼 ID에 해당하는 이미지 URL
+    var imageUrl = images[buttonId];
     var screenImage = document.querySelector('.screen-image');
 
-    screenImage.src = imageUrl; // 이미지 소스 변경
-
-    // 이전 확대 애니메이션 효과 제거
+    screenImage.src = imageUrl;
     screenImage.classList.remove('zoom-effect');
 
-    // 브라우저가 이전 변경 사항을 무시하지 않도록 함
     setTimeout(() => {
-        // 확대 애니메이션 추가
         screenImage.classList.add('zoom-effect');
-    }, 10); // 딜레이 후 애니메이션 추가
-
-    // "뒤로 가기" 및 "새 이미지" 버튼 숨기기
-    document.getElementById('backButton').classList.add('hidden');
-    document.getElementById('newImageButton').classList.add('hidden');
-
-    // 첫 번째 화면의 버튼들 다시 보이기
-    document.querySelectorAll('.btn').forEach(button => button.classList.remove('hidden'));
+    }, 10);
 }
 
-// 뒤로 가는 버튼 기능
 document.getElementById('backButton').onclick = function() {
+    // 초기 화면으로 돌아갔을 때, 초기 화면의 버튼들을 다시 보이게 하고, 자신을 숨김
     var screenImage = document.querySelector('.screen-image');
-    // 초기 화면 이미지 URL로 변경
-    screenImage.src = '최아들배경.PNG';
-    this.classList.add('hidden');
-    document.getElementById('newImageButton').classList.add('hidden');
-    // 첫 번째 화면의 버튼들 다시 보이기
-    document.querySelectorAll('.btn').forEach(button => button.classList.remove('hidden'));
-
-    // 확대 애니메이션 효과 제거
+    screenImage.src = '최아들배경.PNG'; // 초기 화면 이미지로 변경
+    document.querySelectorAll('.btn').forEach(button => button.classList.remove('hidden')); // 버튼들을 다시 보이게 함
+    this.classList.add('hidden'); // 뒤로 가기 버튼 숨기기
+    document.getElementById('newImageButton').classList.add('hidden'); // 새 이미지 버튼 숨기기
     screenImage.classList.remove('zoom-effect');
 };
 
